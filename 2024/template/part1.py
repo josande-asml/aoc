c71cf1b1104c3ae7f5e0cd8bef26295d10aa2c2e
Joris van der Sande <joris.van.der.sande@asml.com>
2024-12-19 23:35:02 +0100

    Update template to include unittests

--- 2024/template/part1.py
+++ 2024/template/part1.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
-f = open('example.txt')
-#f = open('input.txt')
+def process(filename):
+   # Load grid
+   grid = []
+   for line in open(filename):
+      line = line.strip()
+      row = [ c for c in line ]
+      grid += [ row ]
+   H = len(grid)
+   W = len(grid[0])
+   
+   # Load lines like: "123: 234 345 456 567 678"
+   for line in open(filename):
+      parts = re.split(':', line.strip())
+      target = int(parts[0])
+      values = [int(x) for x in parts[1].split()]
 
-# Load grid
-grid = []
-for line in f:
-   line = line.strip()
-   row = [ c for c in line ]
-   grid += [ row ]
-H = len(grid)
-W = len(grid[0])
+   sum = 0
+   print(filename, sum)
+   return sum
 
-# Load lines like: "123: 234 345 456 567 678"
-for line in f:
-	parts = re.split(':', line.strip())
-	target = int(parts[0])
-	values = [int(x) for x in parts[1].split()]
+
+assert(process("example.txt") == 0)
+assert(process("input.txt") == 0)
